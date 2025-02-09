d72839192f74d48ed232d68fbf1c64c4ca3d1cdd
Samarth Patil <samarthpatil2022.comp@mmcoe.edu.in>
2025-02-09 21:47:39 +0530

    cards done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,49 @@
+import StartupCard from "@/components/startupcard";
+import SearchForm from "../../components/searchform";
+
+export default async function Home({searchParams} : {
+  searchParams: Promise<{query?:string}>
+}) {
+    const query = (await searchParams).query;
+
+    const posts = [
+      {
+        _createdAt: new Date(),
+        views: 55,
+        author: { _id: 1, name: "Andrew"},
+        _id: 1,
+        description: "This is a description",
+        image: "https://www.indiafilings.com/learn/wp-content/uploads/2023/02/What-is-a-startup-business.jpg",
+        category: "Robots",
+        title: "We Robots",
+      }
+    ]
+
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br /> Connect With Entrepreneurs</h1>
+
+        <p className="sub-heading !max-w-3xl">Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitons.</p>
+
+        <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+            {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={posts?._id} post={post} />
+              ))
+            ) : (
+              <p className="no-results">No startups found</p>
+            )}
+        </ul>
+      </section>
+    </>
+  );
+}
